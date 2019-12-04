9f54cbe894a29ee1460624195c0fa5e771e6e685
seishikasai <bluelegend.fire5@gmail.com>
2019-12-04 16:37:15 +0900

    その後

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,7 +4,8 @@ $(function() {
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-      </div>`;
+      </div>
+    `;
     $("#user-search-result").append(html);
   }
 
@@ -12,8 +13,21 @@ $(function() {
     let html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">ユーザーが見つかりません</p>
-      </div>`;
+      </div>
+    `;
     $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
   }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
@@ -37,7 +51,21 @@ $(function() {
         }
       })
       .fail(function() {
-        alert("ユーザー検索に失敗しました");
+        alert("通信エラーです。ユーザーが表示できません。");
       });
+  });
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
   });
 });
