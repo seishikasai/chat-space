fd2e62b0c9be7c9970c843e431d48a088e8c4c70
seishikasai <bluelegend.fire5@gmail.com>
2019-12-06 21:26:08 +0900

    setting cap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+server '52.68.149.57', user: 'ec2-user', roles: %w{app db web}
