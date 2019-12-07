af1446ea2d658386ddd77aa541bbbee2093877b9
seishikasai <bluelegend.fire5@gmail.com>
2019-12-07 23:06:29 +0900

    setting cap2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '52.68.149.57', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
